5af7d17c109a61cd3cddf5d3ff57679bc9577b19
ykozhan <justerkanat@gmail.com>
2025-10-28 23:38:48 +0500

    feat: rideboot

--- internal/ride/bootstrap/compose.go
+++ internal/ride/bootstrap/compose.go
@@ -1,1 +1,11 @@
 package bootstrap
+
+import (
+	"context"
+
+	"ridehail/internal/shared/config"
+	"ridehail/internal/shared/logger"
+)
+
+func Run(ctx context.Context, cfg config.Config, log *logger.Logger) {
+}
